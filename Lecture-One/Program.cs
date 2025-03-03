2d4c6301274f6c97655a00ce0bca03adbbe1186d
ENG.ABDALLAH <ENG.ABDALLAH@DESKTOP-TT99K9O>
2025-03-03 06:15:58 +0200

    Add project files.

---
+++ Lecture-One/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lecture_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tax = 6.6;
+            int smallRoom = 25;
+            int largeRoom = 35;
+
+
+            Console.WriteLine($"Islam's Carpet Cleaning Service");
+
+            Console.Write($"Please Enter Number Of Small Rooms : ");
+            int numSmallRooms = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Please Enter Number Of Large Rooms : ");
+            int numLargeRooms = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Price per small room : {smallRoom:c}");
+            Console.WriteLine($"Price per large room : {largeRoom:c}");
+
+            double cost = (numSmallRooms * smallRoom) + (numLargeRooms * largeRoom);
+            Console.WriteLine($"Cost : {cost:c}");
+            Console.WriteLine($"Tax : {tax:c}");
+            Console.WriteLine("======================================");
+
+            Console.WriteLine($"Total estimate : {tax + cost:c}");
+            Console.WriteLine("Estimates are valid for 30 days");
+        }
+    }
+}
